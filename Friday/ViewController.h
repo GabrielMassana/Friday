3deaa5b4e24da0a280fba4788495b7d3aed463f7
Gabriel Massana <gabriel.massana@irisvalet.com>
2014-01-14 11:33:51 +0000

    Initial Commit

---
+++ Friday/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Friday
+//
+//  Created by Gabriel Massana on 1/14/14.
+//  Copyright (c) 2014 Gabriel Massana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Friday/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Friday
+//
+//  Created by Gabriel Massana on 1/14/14.
+//  Copyright (c) 2014 Gabriel Massana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
